c16af55266f6c4b1489fc5c65512ba035e5b81da
Tushar Reddy <reddytushar295@outlook.com>
2024-10-09 16:34:52 +0530

    First Commit

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Flask API</h1><p>This site is a prototype API for a Flask application.</p>"
+
+if __name__ == '__main__':
+    app.run()
